4d5d4bed463344a9bd43033e5f169d048c450aba
Suhasnama <suhasnama23@gmail.com>
2021-07-18 19:40:46 +0530

    Added Header, Button, Tasks and Task component.

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>{{ msg }}</h1>
+    <Button color="black" text="Add task"></Button>
+  </header>
+</template>
+    
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  props: {
+    msg: String,
+  },
+  components: { Button },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
